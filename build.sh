6d13d1639516f5e8699ee3f621349a9a205240c8
Sculas <contact@sculas.xyz>
2022-06-15 22:31:10 +0200

    fix: fine then

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 cd termux-packages
-./scripts/run-docker.sh ../setup.sh
+mv ../setup.sh .
+./scripts/run-docker.sh ./setup.sh
 for arch in "aarch64" "arm" "i686"; do
   echo "Building aapt for $arch"
   ./scripts/run-docker.sh ./build-package.sh -I -a $arch aapt

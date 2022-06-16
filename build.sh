af77ac0826b02e28c45fbcd669d3dded45d1298d
Lucaskyy <contact@sculas.xyz>
2022-06-16 20:53:04 +0200

    fix: hmm?

--- build.sh
+++ build.sh
@@ -4,10 +4,14 @@ function buildAapt() {
     --ndk="$NDK_TOOLCHAIN" \
     --arch "$arch" \
     --build "dist/$arch" \
-    --target aapt2
+    --target aapt2 || exit 1
 }
 
-echo "Downloading sources.."
+echo "Installing dependencies..."
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install build-essential unzip nasm cmake ninja-build -y || exit 1
+echo "Downloading sources..."
 wget https://github.com/Lzhiyong/sdk-tools/releases/download/33.0.1/sdk-tools-source.zip -q -O sdk-tools-source.zip
 unzip -q sdk-tools-source.zip
 cd sdk-tools-source

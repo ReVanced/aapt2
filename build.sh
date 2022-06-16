928653b284f0acb6f65a63711bb11116ac1f3496
Lucaskyy <contact@sculas.xyz>
2022-06-16 20:49:38 +0200

    fix: uh?

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ function buildAapt() {
 
 echo "Downloading sources.."
 wget https://github.com/Lzhiyong/sdk-tools/releases/download/33.0.1/sdk-tools-source.zip -q -O sdk-tools-source.zip
-unzip sdk-tools-source.zip -q
+unzip -q sdk-tools-source.zip
 cd sdk-tools-source
 
 for arch in "aarch64" "arm" "x86"; do

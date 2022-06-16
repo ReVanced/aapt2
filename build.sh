bccc507d20b7a3de277efbeb17206e34359ca435
Lucaskyy <contact@sculas.xyz>
2022-06-16 20:45:08 +0200

    fix: made an oopsie

--- build.sh
+++ build.sh
@@ -1,13 +1,3 @@
-echo "Downloading sources.."
-wget https://github.com/Lzhiyong/sdk-tools/releases/download/33.0.1/sdk-tools-source.zip -O sdk-tools-source.zip
-unzip sdk-tools-source.zip
-cd sdk-tools-source
-
-for arch in "aarch64" "arm" "x86"; do
-  echo "Building aapt for $arch"
-  buildAapt $arch
-done
-
 function buildAapt() {
   arch=$1
   python3 build.py \
@@ -16,3 +6,13 @@ function buildAapt() {
     --build "dist/$arch" \
     --target aapt2
 }
+
+echo "Downloading sources.."
+wget https://github.com/Lzhiyong/sdk-tools/releases/download/33.0.1/sdk-tools-source.zip -q -O sdk-tools-source.zip
+unzip sdk-tools-source.zip -q
+cd sdk-tools-source
+
+for arch in "aarch64" "arm" "x86"; do
+  echo "Building aapt for $arch"
+  buildAapt $arch
+done
